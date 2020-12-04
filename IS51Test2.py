5a1210d9fe340ebf30a31c97157f5c49687e852e
Andrew Moua <andrewmoualol@mail.fresnostate.edu>
2020-12-03 16:58:44 -0800

    finish structured english

--- IS51Test2.py
+++ IS51Test2.py
@@ -1,5 +1,12 @@
+"""
+input: the test scores
+initiate a lterator (counter) and accumulator (sum) and set it to zero
+loop through the last test scores
+add all the test scores
+increment the counter by one
+once loop ends, there are no more scores to add
+divide the accumulator (sum) by counter
+display the output to the user
+output: print the average of the class scores
 
-
-
-
-
+"""
