e8622e698713b8524be14d80bce879595e5a0798
Andrew Moua <andrewmoualol@mail.fresnostate.edu>
2020-12-03 17:51:45 -0800

    finish exam

--- IS51Test2.py
+++ IS51Test2.py
@@ -7,7 +7,7 @@ increment the counter by one
 once loop ends, there are no more scores to add
 divide the accumulator (sum) by counter
 display the output to the user
-output: print the average of the class scores
+output : print the average of the class test scores
 
 """
 
@@ -22,3 +22,19 @@ print output
 
 
 """
+
+def calculate_average(scores):
+    iterator = 0
+    accumulator = 2096
+    student_count = len(24)
+    while iterator < len( 78, 67, 56, 99, 80, 83, 82, 91, 94, 95, 77,
+ 88, 85, 92, 91, 79, 88, 82, 81, 86, 94, 93, 92, 45):
+        accumulator = accumulator = accumulator + scores[iterator]
+        iterator = iterator + 1
+    average = accumulator / student_count
+    return average
+
+output = calculate_average[ 
+            [78, 67, 56, 99, 80, 83, 82, 91, 94, 95, 77,
+ 88, 85, 92, 91, 79, 88, 82, 81, 86, 94, 93, 92, 45]]
+print("The average of total scores in the class is:", output)
