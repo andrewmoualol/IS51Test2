a4d30233a0b683ce7860bc72681abecb7025448d
Andrew Moua <andrewmoualol@mail.fresnostate.edu>
2020-12-03 17:08:40 -0800

    finish pseudocode

--- IS51Test2.py
+++ IS51Test2.py
@@ -1,6 +1,6 @@
 """
 input: the test scores
-initiate a lterator (counter) and accumulator (sum) and set it to zero
+initiate a iterator (counter) and accumulator (sum) and set it to zero
 loop through the last test scores
 add all the test scores
 increment the counter by one
@@ -8,5 +8,17 @@ once loop ends, there are no more scores to add
 divide the accumulator (sum) by counter
 display the output to the user
 output: print the average of the class scores
+
+"""
+
+"""
+scores
+iterator, accumulator = 0
+loop through scores
+    accumulator = accumulator + scores
+    iterator = iterator + 1
+output = accumulator / total scores
+print output
+
 
 """
